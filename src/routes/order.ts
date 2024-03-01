6e65497bbd876ad526e9742c3cb8614ad154604d
alesssha <159577408+alesssha@users.noreply.github.com>
2024-03-01 09:59:28 -0800

    Initial commit

---
+++ src/routes/order.ts
@@ -0,0 +1,6 @@
+import { Router } from "express";
+import { getItems } from "../controllers/order";
+import { checkJwt } from "../middleware/session";
+const router = Router();
+router.get("/", checkJwt, getItems);
+export { router };
